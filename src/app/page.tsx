16f39bfe987db4011d75303239a082e80f823b2d
Approximations <guilhermeksilva556@gmail.com>
2023-11-05 03:24:42 -0300

    progress on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import Image from 'next/image';
+import { data } from '@/data/DataReader';
 
 export default function Home() {
-    return <div>Porra!</div>;
+    return (
+        <section className="relative mt-24 p-7 text-light-gray">
+            <span className="relative z-10">Hi all. I am</span>
+            <h1 className="relative z-10 text-5xl leading-[56px]">{data.name}</h1>
+            <div className="absolute top-0 z-0 h-32 w-32 bg-accent-turquoise blur-[80px]"></div>
+            <div className="absolute left-40 top-32 h-32 w-32 bg-secondary-indigo blur-[80px]"></div>
+            <h3 className="mt-1 text-xl text-accent-turquoise">{`> ${data.role}`}</h3>
+        </section>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,12 @@ const config: Config = {
                 'blue-2': '#011627',
                 'blue-3': '#1E2D3D',
                 'blue-4': '#607B96',
+                'light-gray': '#E5E9F0',
+                'accent-turquoise': '#43D9AD',
+                'secondary-indigo': '#4D5BCE',
+            },
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
             },
         },
     },
