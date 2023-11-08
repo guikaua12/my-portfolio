db6a7eadef98bf4f6072dd9a1ff8dfdadf2eda64
Approximations <guilhermeksilva556@gmail.com>
2023-11-08 00:24:52 -0300

    complete Footer

--- src/feature/Footer/index.tsx
+++ src/feature/Footer/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import { data } from '@/data/DataReader';
+import Icon from '../../components/Icon';
+import Link from 'next/link';
 
 const Footer = () => {
-    return <footer className="border-t border-t-blue-3 p-4">a</footer>;
+    const findMeAlsoIn = data.contacts.find_me_also_in;
+
+    return (
+        <footer className="flex justify-between border-t border-t-blue-3">
+            <span className="p-4">{findMeAlsoIn.title}:</span>
+
+            <div className="flex">
+                {findMeAlsoIn.sources.map((source) => (
+                    <div className="flex items-center justify-center border-l border-l-blue-3 px-4">
+                        <Link href={source.url}>
+                            <Icon name={source.icon} size={28} opacity={0.4} />
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
