530d0700b2f1211d932be0c870850545828a87d0
Approximations <guilhermeksilva556@gmail.com>
2023-11-08 00:39:47 -0300

    make header logo clickable

--- src/feature/Header/index.tsx
+++ src/feature/Header/index.tsx
@@ -2,11 +2,17 @@
 import React from 'react';
 import { data } from '@/data/DataReader';
 import HeaderMenuButton from '@/feature/Header/Menu/Button';
+import Link from 'next/link';
+import { useHeaderMenu } from '@/feature/Header/Menu/hook/useHeaderMenu';
 
 const Header = () => {
+    const { closeMenu } = useHeaderMenu();
+
     return (
         <header className="flex w-full justify-between border-b border-b-blue-3 p-4">
-            <span>{data.logo_name}</span>
+            <Link href="/" onClick={closeMenu}>
+                {data.logo_name}
+            </Link>
             <HeaderMenuButton />
         </header>
     );
