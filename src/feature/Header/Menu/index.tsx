bf1fa551685846ad4484af48ed930240716fe76c
Approximations <guilhermeksilva556@gmail.com>
2023-11-08 00:40:10 -0300

    close header menu on click on any its links

--- src/feature/Header/Menu/index.tsx
+++ src/feature/Header/Menu/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import { useHeaderMenu } from '@/feature/Header/Menu/hook/useHeaderMenu';
 
 const HeaderMenu = () => {
+    const { closeMenu } = useHeaderMenu();
+
     return (
         <nav className="flex-1 bg-blue-2">
-            <ul className="flex flex-col text-white">
+            <ul className="flex flex-col text-white" onClick={closeMenu}>
                 <li>
                     <Link href="#" className="inline-block w-full border-b border-b-blue-3 p-4">
                         _hello
