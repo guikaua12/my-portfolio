da70a5a4fa91211bcc4a288acf746ef3ff580b96
Approximations <guilhermeksilva556@gmail.com>
2023-11-09 23:40:08 -0300

    set active data when click in any file

--- src/feature/AboutMe/Section/Info/File/index.tsx
+++ src/feature/AboutMe/Section/Info/File/index.tsx
@@ -2,14 +2,28 @@
 import React, { ComponentProps } from 'react';
 import { AboutSectionInfoFile } from '@/data/DataReader';
 import MIcon from '@/components/MIcon';
+import { useActiveData } from '@/feature/AboutMe/Section/ActiveData/hook/useActiveData';
 
 interface Props extends ComponentProps<'div'> {
     file: AboutSectionInfoFile;
 }
 
 const AboutMeSectionInfoFile = ({ file, ...rest }: Props) => {
+    const { setActiveData } = useActiveData();
+    const handleFileClick = () => {
+        setActiveData({
+            section: file.parent.parent,
+            sectionInfo: file.parent,
+            infoFile: file,
+        });
+    };
+
     return (
-        <div className="flex w-full cursor-pointer items-center gap-3 py-1 pl-16 text-blue-4" {...rest}>
+        <div
+            className="flex w-full cursor-pointer items-center gap-3 py-1 pl-16 text-blue-4"
+            onClick={handleFileClick}
+            {...rest}
+        >
             <MIcon />
             {file.name}
         </div>
