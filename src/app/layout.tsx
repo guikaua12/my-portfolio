aa8a8cbedcdecbdbcb134af3b32d8243f3fe06bd
Approximations <guilhermeksilva556@gmail.com>
2023-11-17 13:04:58 -0300

    fixed div not growing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <body
                     className={`${fira_code.className} min-h-full w-full items-center bg-blue-1 p-4 text-blue-4 lg:flex lg:p-0`}
                 >
-                    <div className="flex min-h-[calc(100vh-2rem)] flex-col rounded-lg border bg-blue-2 lg:mx-16 lg:h-[calc(100vh-8rem)] lg:min-h-max lg:overflow-hidden">
+                    <div className="flex min-h-[calc(100vh-2rem)] flex-col rounded-lg border bg-blue-2 lg:mx-16 lg:h-[calc(100vh-8rem)] lg:min-h-max lg:flex-grow lg:overflow-hidden">
                         <Header />
                         <WithMobileHeaderMenu>{children}</WithMobileHeaderMenu>
                         <Footer />
