6fd3448da3b55ede890c255a94706b24a15e6017
Approximations <guilhermeksilva556@gmail.com>
2023-11-19 21:41:20 -0300

    remove useless class

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -6,7 +6,7 @@ import Gists from '@/feature/AboutMe/Gists';
 
 const AboutMePage = () => {
     return (
-        <Page className="lg: flex-grow overflow-auto lg:flex" title="_about-me">
+        <Page className="flex-grow overflow-auto lg:flex" title="_about-me">
             <Sections />
 
             <AboutMeActiveData />
